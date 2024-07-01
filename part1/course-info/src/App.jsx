0f51c2ba5e2b3397f00d79f303f46df03349cfcd
Anna Riazantseva <anyariazantzeva@yandex.ru>
2024-07-01 14:24:36 +0300

    Init course-info + 1.1 exercise

---
+++ part1/course-info/src/App.jsx
@@ -0,0 +1,48 @@
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({partOne, partTwo, partThree, exercisesOne, exercisesTwo, exercisesThree}) => {
+  return (
+    <>
+       <p>
+        {partOne} {exercisesOne}
+      </p>
+      <p>
+        {partTwo} {exercisesTwo}
+      </p>
+      <p>
+        {partThree} {exercisesThree}
+      </p>
+    </>
+  )
+}
+
+const Total = ({exercisesOne, exercisesTwo, exercisesThree }) => {
+  return (
+    <p>Number of exercises {exercisesOne + exercisesTwo + exercisesThree}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content partOne={part1} partTwo={part2} partThree={part3} exercisesOne={exercises1} exercisesTwo={exercises2} exercisesThree={exercises3}/>
+      <Total exercisesOne={exercises1} exercisesTwo={exercises2} exercisesThree={exercises3}/>
+      
+    </div>
+  )
+}
+
+export default App
