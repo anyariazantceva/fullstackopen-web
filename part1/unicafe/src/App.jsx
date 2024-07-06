098819bbac04aecf537afcf082caa3a432e511ff
Anna Riazantseva <anyariazantzeva@yandex.ru>
2024-07-06 13:24:50 +0300

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,13 +2,47 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  let [good, setGood] = useState(0)
+  let [neutral, setNeutral] = useState(0)
+  let [bad, setBad] = useState(0)
+  let total = good + bad + neutral;
+
+  const addStatistics = (statsType) => {
+    switch (statsType) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;  
+      default:
+        break;
+    }
+  }
+
+  const calculateAverage = () => {
+    return good * 1 + neutral * 0 + bad * (-1)/ total;
+  }
 
   return (
     <div>
-      code here
+      <h2>Give feedback</h2>
+      <button onClick={() => addStatistics("good")}>good</button>
+      <button onClick={() => addStatistics("neutral")}>neutral</button>
+      <button onClick={() => addStatistics("bad")}>bad</button>
+
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {good + bad + neutral}</p>
+        <p>Average: {calculateAverage() || 0}</p>
+        <p>Positive: {`${parseFloat(good / total) * 100 } %`}</p>
+      </div>
     </div>
   )
 }
