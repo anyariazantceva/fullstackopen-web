1a6bb04b17840273f285b66bce524279e512d83f
Anna Riazantseva <anyariazantzeva@yandex.ru>
2024-09-27 20:03:55 +0300

    3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -32,6 +32,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const dateStr = new Date().toString();
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${dateStr}</p>`)
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
